12d3242911ed8870f94f6bd75b00b5420b4fa758
Thijs-de-Bruin <thijsdebruin5@gmail.com>
2024-06-06 12:50:18 +0200

    command file creation

--- algorithm_prep.py
+++ algorithm_prep.py
@@ -3,11 +3,28 @@ This file is meant for making a .cmd file that let's the algorithm several times
 """
 import os
 
+commands_file_name = "execute_all_cases.sh"
+all_cutoff_iterations = [0.01, 0.02, 0.03] # @Yanna
+all_cutoff_phases = [0.0, 0.1, 0.2] # @Yanna
+input_file_name = "test.txt"
+
 def float_to_string(number: float):
     return str(number).replace('.', 'p')
 
-def generate_commandline(inputFileName: str, cutoff_iterations: float = 0.01, cutoff_phases: float = 0.0):
-    return (f"./biLouvain/src/biLouvain -i input/{inputFileName} -d \",\" -ci {cutoff_iterations} -cp {cutoff_phases} -o " +
-            f" output/ResultsCt{float_to_string(cutoff_iterations)}Cp{float_to_string(cutoff_phases)}")
+def generate_commandline(inputFileName: str, cutoff_iteration: float = 0.01, cutoff_phase: float = 0.0):
+    test_prefix = "Test" if inputFileName == "test.txt" else ""
+    return (f"./biLouvain/src/biLouvain -i input/{inputFileName} -d \",\" -ci {cutoff_iteration} -cp {cutoff_phase} -o " +
+            f" output/{test_prefix}ResultsCt{float_to_string(cutoff_iteration)}Cp{float_to_string(cutoff_phase)}")
+
+if os.path.exists(commands_file_name):
+    os.remove(commands_file_name)
+
+commands_file = open(commands_file_name, "a")
+
+for cutoff_iteration in all_cutoff_iterations:
+    for cutoff_phase in all_cutoff_phases:
+        commands_file.write(generate_commandline(input_file_name, cutoff_iteration, cutoff_phase) + "\n")
+
+commands_file.close()
 
 print(generate_commandline("test.txt"))
---
+++ execute_all_cases.sh
@@ -0,0 +1,2 @@
+./biLouvain/src/biLouvain -i input/test.txt -d "," -ci 0.01 -cp 0.0 -o  output/ResultsCt0p01Cp0p0
+./biLouvain/src/biLouvain -i input/test.txt -d "," -ci 0.02 -cp 0.0 -o  output/ResultsCt0p02Cp0p0
