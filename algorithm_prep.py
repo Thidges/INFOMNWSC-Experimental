87319f56f6673592d521c28bfe6fbe8c0ea2e71f
Thijs-de-Bruin <thijsdebruin5@gmail.com>
2024-06-06 10:33:08 +0200

    Working version of running algorithm

--- algorithm_prep.py
+++ algorithm_prep.py
@@ -3,6 +3,11 @@ This file is meant for making a .cmd file that let's the algorithm several times
 """
 import os
 
-def generate_commandline(inputFileName: str, cutoff_iterations: float = 0.01, cutoff_phases: float = 0.0):
-    return f"./biLouvain -i {inputFileName}, id \",\" -ci {cutoff_iterations} -cp {cutoff_phases}"
+def float_to_string(number: float):
+    return str(number).replace('.', 'p')
 
+def generate_commandline(inputFileName: str, cutoff_iterations: float = 0.01, cutoff_phases: float = 0.0):
+    return (f"./biLouvain/src/biLouvain -i input/{inputFileName} -d \",\" -ci {cutoff_iterations} -cp {cutoff_phases} -o " +
+            f" output/ResultsCt{float_to_string(cutoff_iterations)}Cp{float_to_string(cutoff_phases)}")
+
+print(generate_commandline("test.txt"))
