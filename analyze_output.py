119c2808613dc695455baf693b878cb6b6017c77
Thijs-de-Bruin <thijsdebruin5@gmail.com>
2024-06-09 09:26:25 +0200

    Updated output

--- analyze_output.py
+++ analyze_output.py
@@ -5,6 +5,12 @@ import os
 
 output_files = os.listdir("output")
 
+for file_name in output_files:
+    if file_name[-21:] == "ResultsModularity.txt":
+        "ResultsCt0p0001Cp0p001_ResultsModularity.txt"
+
+# TODO: modularity uitlezen
+
 header = ["Iteration cutoff", "Phase cutoff", "Murata+ Modularity", "Willen we hier nog meer?"]
 output_data = [[0.01, 0.0, 0.4555, "iets ofzo"], [0.01, 0.001, 0.3955, "iets ofzo"], [0.025, 0.0, 0.5842, "iets ofzo"],
                [0.025, 0.001, 0.4842, "iets ofzo"]] # Iedere index in output_data heeft de informatie van de header, nu met mock data
--- execute_all_cases.sh
+++ execute_all_cases.sh
@@ -1,8 +1,12 @@
-./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.0001 -cp 0.0 -o  output/ResultsCt0p0001Cp0p0
-./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.0001 -cp 0.001 -o  output/ResultsCt0p0001Cp0p001
 ./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.001 -cp 0.0 -o  output/ResultsCt0p001Cp0p0
 ./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.001 -cp 0.001 -o  output/ResultsCt0p001Cp0p001
 ./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.01 -cp 0.0 -o  output/ResultsCt0p01Cp0p0
 ./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.01 -cp 0.001 -o  output/ResultsCt0p01Cp0p001
-./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.03 -cp 0.0 -o  output/ResultsCt0p03Cp0p0
-./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.03 -cp 0.001 -o  output/ResultsCt0p03Cp0p001
+./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.025 -cp 0.0 -o  output/ResultsCt0p025Cp0p0
+./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.025 -cp 0.001 -o  output/ResultsCt0p025Cp0p001
+./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.05 -cp 0.0 -o  output/ResultsCt0p05Cp0p0
+./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.05 -cp 0.001 -o  output/ResultsCt0p05Cp0p001
+./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.075 -cp 0.0 -o  output/ResultsCt0p075Cp0p0
+./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.075 -cp 0.001 -o  output/ResultsCt0p075Cp0p001
+./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.1 -cp 0.0 -o  output/ResultsCt0p1Cp0p0
+./biLouvain/src/biLouvain -i input/pollinator_edges.txt -d "," -ci 0.1 -cp 0.001 -o  output/ResultsCt0p1Cp0p001
